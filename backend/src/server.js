39d8264978de03e07b9abdfc6b7125298ea377b2
Sumedha Thakur <sumedhathakur123@gmail.com>
2026-07-20 18:48:00 +0200

    removing debu statements

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,15 +4,11 @@ require("dotenv").config({
   path: path.resolve(__dirname, "../.env"),
 });
 
-console.log("process.env.PORT =", process.env.PORT);
-
 require("./config/db");
 
 const app = require("./app");
 
 const PORT = process.env.PORT || 5001;
-
-console.log("Using PORT =", PORT);
 
 app.listen(PORT, () => {
   console.log(`Backend running on http://localhost:${PORT}`);
